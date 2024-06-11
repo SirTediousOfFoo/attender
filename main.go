401614d9b9b76f610597a193dbc45397e3863f44
Petar Ivankovic Milosevic <petar.imilosevic@pm.me>
2024-06-11 16:50:35 +0200

    set up basic UI

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+
+import (
+
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Create a custom file server
+	fileServer := http.FileServer(http.Dir("templates/"))
+	// Serve up the index page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/index.html")
+	})
+	// Set the correct MIME type for CSS files
+	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css")
+		fileServer.ServeHTTP(w, r)
+	})
+
+	fmt.Println("Server started on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
