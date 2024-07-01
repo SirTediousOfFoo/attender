ec9e57cc01c56f03b0ff9de033163970ffc204a3
Petar Ivanković Milošević <petar.imilosevic@pm.me>
2024-07-01 16:52:37 +0200

    log attendance, fix weird bugs (#3)
    Signed-off-by: Petar Ivankovic Milosevic <petar.imilosevic@pm.me>

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -71,4 +73,50 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 
 func termsAndConditionsHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/tnc.html")
+}
+
+func attendHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the user has a session cookie
+	userID, err := checkAuthenticated(db, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	user, err := getUserFromDB(db, userID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	// Render the index.gohtml template
+	tmpl, err := template.ParseFiles("templates/attended.gohtml")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Log the attendance in the db
+	_, err = db.Exec("INSERT INTO attendance (userid, date) VALUES ($1, $2)", user.ID, time.Now())
+	if err != nil {
+		if err.Error() == "pq: duplicate key value violates unique constraint \"attendance_unique\"" {
+			tmpl, err = template.ParseFiles("templates/alreadyAttended.gohtml")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			err = tmpl.Execute(w, nil)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Return a random happy message
+	sucessString := []string{"Yay", ":)", "Woo", "Huzzah", "Awesome", "Fantastic", "Yippee", "Alright", "^_^", "Cool", "Cool beans"}
+	err = tmpl.Execute(w, sucessString[rand.Intn(len(sucessString))])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
